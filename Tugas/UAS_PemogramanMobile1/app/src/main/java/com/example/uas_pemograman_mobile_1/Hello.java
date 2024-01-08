da193af6c87a165e39aa9626e195efd2f51d8f93
Rico Prasetya <ricoprasetya24@gmail.com>
2024-01-09 06:42:08 +0700

    menambah tugas

---
+++ Tugas/UAS_PemogramanMobile1/app/src/main/java/com/example/uas_pemograman_mobile_1/Hello.java
@@ -0,0 +1,15 @@
+package com.example.uas_pemograman_mobile_1;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Hello extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello);
+
+    }
+}
